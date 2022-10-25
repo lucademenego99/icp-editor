0c16837ff2146d07a803408243ab91a28e94bbba
Luca De Menego <lucademenego99@gmail.com>
2022-10-25 11:50:45 +0200

    Updates

--- assets/quill.f2e9192b.js
+++ assets/quill.f2e9192b.js
@@ -1,4 +1,4 @@
-import{g as pt,c as yt}from"./index.62c1641e.js";function mt(ct,ht){for(var B=0;B<ht.length;B++){const _=ht[B];if(typeof _!="string"&&!Array.isArray(_)){for(const v in _)if(v!=="default"&&!(v in ct)){const P=Object.getOwnPropertyDescriptor(_,v);P&&Object.defineProperty(ct,v,P.get?P:{enumerable:!0,get:()=>_[v]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var dt={exports:{}};/*!
+import{g as pt,c as yt}from"./index.441dd8c2.js";function mt(ct,ht){for(var B=0;B<ht.length;B++){const _=ht[B];if(typeof _!="string"&&!Array.isArray(_)){for(const v in _)if(v!=="default"&&!(v in ct)){const P=Object.getOwnPropertyDescriptor(_,v);P&&Object.defineProperty(ct,v,P.get?P:{enumerable:!0,get:()=>_[v]})}}}return Object.freeze(Object.defineProperty(ct,Symbol.toStringTag,{value:"Module"}))}var dt={exports:{}};/*!
  * Quill Editor v1.3.7
  * https://quilljs.com/
  * Copyright (c) 2014, Jason Chen
